ab26f847ebcab42f8fab258d6ef97236295620b2
Benia <beniab333@gmail.com>
2023-02-28 21:20:56 +0100

    last problem

--- day13/Debugging.py
+++ day13/Debugging.py
@@ -28,7 +28,7 @@ age = int(input("How old are you?"))
 if age > 18:
     print(f"You can drive at age {age}.")
 
-#Print is Your Friend
+# Print is Your Friend
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
@@ -36,12 +36,14 @@ word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-#Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-  b_list.append(new_item)
-  print(b_list)
 
-mutate([1,2,3,5,8,13])
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
