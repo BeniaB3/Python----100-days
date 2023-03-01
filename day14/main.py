4fc6258f5741faf032558a0385809a0da6697156
Benia <beniab333@gmail.com>
2023-03-01 15:38:48 +0100

    random person, compare and who_has_more

--- day14/main.py
+++ day14/main.py
@@ -4,3 +4,20 @@ import random
 
 print(art.logo)
 
+
+def random_person():
+    return random.choice(game_data.data)
+
+
+def compare(person1, person2):
+    while person1 == person2:
+        person2 = random_person()
+    return person2
+
+
+def who_has_more(person1, person2):
+    if person1["follower_count"] > person2['follower_count']:
+        return person1
+    else:
+        return person2
+
