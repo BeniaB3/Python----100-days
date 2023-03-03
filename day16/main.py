6811f53ff0783e55c984fc3659c54f4f691a03a2
Benia <beniab333@gmail.com>
2023-03-03 19:57:15 +0100

    main

--- day16/main.py
+++ day16/main.py
@@ -9,19 +9,19 @@ money_machine = MoneyMachine()
 
 def get_order():
     """Get order from user"""
-    order = input(f"What would you like? ({menu.get_items()}): ")
-    return order
+    c_order = input(f"What would you like? ({menu.get_items()}): ")
+    return c_order
 
 
-def make_order(order):
-    if order == 'off':
+def make_order(c_order):
+    if c_order == 'off':
         return False
-    elif order == 'report':
+    elif c_order == 'report':
         coffee_maker.report()
         money_machine.report()
         return True
     else:
-        drink = menu.find_drink(order)
+        drink = menu.find_drink(c_order)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
         return True
