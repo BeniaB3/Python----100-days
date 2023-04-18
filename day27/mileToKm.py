283d30ce23fca5ad86183b16fba861f78cc2d71d
Benia <beniab333@gmail.com>
2023-04-18 17:42:12 +0200

    mileToKM

--- day27/mileToKm.py
+++ day27/mileToKm.py
@@ -2,13 +2,13 @@ from tkinter import *
 
 
 def miles_to_km():
-    miles = float(entry.get())
+    miles = float(miles_input.get())
     km = miles * 1.609
     return km
 
 
 def change_label3():
-    label3.config(text=miles_to_km())
+    zero_label.config(text=miles_to_km())
 
 
 window = Tk()
@@ -16,22 +16,23 @@ window.title("Mile to Km Converter")
 window.minsize(width=300, height=100)
 window.config(padx=100, pady=50)
 
-entry = Entry(width=10)
-entry.grid(column=1, row=0)
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
 
-label = Label(text="Miles")
-label.grid(column=2, row=0)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
-label2 = Label(text="is equal to")
-label2.grid(column=0, row=1)
+equal_label = Label(text="is equal to")
+equal_label.grid(column=0, row=1)
 
-label3 = Label(text="0")
-label3.grid(column=1, row=1)
+zero_label = Label(text="0")
+zero_label.grid(column=1, row=1)
 
-label4 = Label(text="Km")
-label4.grid(column=2, row=1)
 
-button = Button(text="Calculate", command=change_label3)
-button.grid(column=1, row=2)
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+calcul_button = Button(text="Calculate", command=change_label3)
+calcul_button.grid(column=1, row=2)
 
 window.mainloop()
